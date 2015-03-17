2bf1a0605ccc2c162116cd8a2321526d7a7d2524
ricgruber <ric@strategiamedia.com>
2015-03-17 16:17:03 -0500

    first committee
    check check

---
+++ plugins/config.php
@@ -0,0 +1,38 @@
+<?php
+
+/* Contact Form
+ ---------------------------------------------------------------------- */
+ 
+// Enter your email address
+$your_email_address = 'info@lesmusicllc.com';
+
+
+/* Twitter Feed
+ ---------------------------------------------------------------------- */
+// For more information on Twitter integration, you can find in theme documentation.
+// Twitter account name
+$username = 'lesmusicllc';
+
+// Twitter consumer key
+$consumer_key = 'SGb6DL8Ic5fSlMkvSWjddewEH';
+
+// Twitter consumer secret key
+$consumer_secret = 'gKscP4IG054tYIgpz0ECx6j9SLYS00OUtIIUvoBYujS1W4o0FF';
+
+// Twitter access token
+$access_token = '1496083946-M9S0jp1IMcOUXlWBsQB0s9jAynsFXwtg5VuqC9O';
+
+// Twitter access token secret
+$access_token_secret = 'iLiuZjoAo2QezBDKij2TaBXXyNSZgejiHRBD4y0n4EjiS';
+
+// Do you want to show replies
+$ignore_replies = true;
+
+// Human time e.g.: "about 1 hour ago"
+$twitter_style_dates = true;
+
+// Date setting if $twitter_style_dates is false
+$date_format = 'F d, Y';
+
+// Twitter cache time (default is 1 hour)
+$twitter_cachetime = 60*60;
